108643b79163de207e8cab3801091a57ab533469
abtark <tarkrimhasan@gmail.com>
2026-01-24 17:22:17 +0600

    layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Ubuntu } from "next/font/google";
+import "./globals.css";
+
+const ubuntu = Ubuntu({ 
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+  variable: '--font-ubuntu',
+});
+
+export const metadata: Metadata = {
+  title: "Emanistation Dashboard",
+  description: "Modern Glassmorphism Dashboard",
+  icons: {
+    icon: "https://iili.io/FC3fr7f.png",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={ubuntu.className}>{children}</body>
+    </html>
+  );
+}
