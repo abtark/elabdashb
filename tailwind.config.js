e4d8a2307c4655def48e370a34f0cbd396719ccb
abtark <tarkrimhasan@gmail.com>
2026-01-24 17:21:11 +0600

    tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['var(--font-ubuntu)'],
+      },
+      colors: {
+        glass: "rgba(255, 255, 255, 0.1)",
+        glassBorder: "rgba(255, 255, 255, 0.2)",
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'hero-glow': 'conic-gradient(from 180deg at 50% 50%, #2a8af6 0deg, #a853ba 180deg, #e92a67 360deg)',
+      },
+    },
+  },
+  plugins: [],
+};
