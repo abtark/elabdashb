d5227cb8bf12250604033bb966d9db1d61afe45a
abtark <tarkrimhasan@gmail.com>
2026-01-24 17:49:46 +0600

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,12 @@
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
         ubuntu: ['var(--font-ubuntu)'],
-      },
-      backgroundImage: {
-        'glass-gradient': 'linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0.05))',
-      },
-      boxShadow: {
-        'glass': '0 8px 32px 0 rgba(0, 0, 0, 0.37)',
       },
     },
   },
