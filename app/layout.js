6164692520a5fe58c424ac24620fc70065a77bf9
abtark <tarkrimhasan@gmail.com>
2026-01-24 17:38:38 +0600

    layout.js

---
+++ app/layout.js
@@ -0,0 +1,28 @@
+import { Ubuntu } from "next/font/google";
+import "./globals.css";
+
+const ubuntu = Ubuntu({ 
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+  variable: "--font-ubuntu",
+});
+
+export const metadata = {
+  title: "Emanistation Dashboard",
+  description: "Modern Glassmorphism Dashboard",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link 
+          rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" 
+        />
+        <link rel="icon" href="https://iili.io/FC3fr7f.png" />
+      </head>
+      <body className={ubuntu.className}>{children}</body>
+    </html>
+  );
+}
